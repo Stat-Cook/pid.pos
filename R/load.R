c6f3a0e6072a597a770008a67940888fb36917db
Rob <rzc1@staff.staffs.ac.uk>
2023-02-21 11:57:10 +0000

    Clean up files

--- R/load.R
+++ R/load.R
@@ -1,6 +1,7 @@
 #' @importFrom udpipe udpipe_load_model
 #' @importFrom rappdirs user_data_dir user_cache_dir
 library(rappdirs)
+
 pid.pos_env <- new.env()
 
 pid.pos_env$deault_model_folder <- user_data_dir("pid.pos")
