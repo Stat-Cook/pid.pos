e467b70b92006e34a4f8c1dd02d11a12a3560941
Rob <rzc1@staff.staffs.ac.uk>
2025-03-14 17:00:03 +0000

    Change to 'data_frame_report' method

--- R/report_on_folder.R
+++ R/report_on_folder.R
@@ -1,7 +1,7 @@
 empty.report <- data.frame(
-  `doc_id` = character(0),
-  token = character(0),
-  sentence =character(0),
+  `ID` = character(0),
+  Token = character(0),
+  Sentence =character(0),
   Repeats = integer(0),
   "Affected Columns" = character(0)
 )
@@ -34,24 +34,16 @@ report_on_folder <- function(data_path, report_dir = "Proper Noun Reports",
     frm <- read_data(.file) %>%
       remove_if_exists(to_remove)
 
-    tags <- data_frame_tagger(frm)
+    report <- data_frame_report(frm)
 
     base <- str_replace(base, "\\..*$", ".csv")
 
     noun.report.path <- file.path(report_dir, base)
 
-    if(is.null(tags$`All Tags`)){
+    if(nrow(report) == 0){
       write.csv(empty.report, noun.report.path)
 
     } else {
-      report <- merge(
-        tags$`Proper Nouns`,
-        tags$Sentences,
-        by.x = "doc_id", by.y = "ID"
-      ) %>%
-        select('doc_id', 'token', 'sentence', 'Repeats', 'Affected Columns') %>%
-        arrange(doc_id)
-
       write.csv(report, noun.report.path)
     }
   }
