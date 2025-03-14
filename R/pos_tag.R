4af4b74c6a920e800db4299182d021b9adced48e
Rob <rzc1@staff.staffs.ac.uk>
2025-03-14 16:59:29 +0000

    Move doc_id to vector names

--- R/pos_tag.R
+++ R/pos_tag.R
@@ -1,36 +1,41 @@
-pos_tag <- function(docs, model="english-ewt", pb=NULL, doc_id = NULL){
+pos_tag <- function(docs, 
+                    model="english-ewt", 
+                    .doc_id = NULL, 
+                    catch=T){
   #' @importFrom udpipe udpipe
   #' @importFrom utf8 utf8_encode
   #' @noRd
-  if (is.null(doc_id)){
-    doc_id <- seq_along(docs)
+  if (is.null(.doc_id)){
+    .doc_id <- seq_along(docs)
   }
 
-  if (is.numeric(doc_id)){
-    doc_id <- paste("doc", doc_id, sep = "")
+  if (is.numeric(.doc_id)){
+    .doc_id <- paste("doc", .doc_id, sep = "")
   }
 
   utf8_docs <- utf8_encode(docs)
-
-  tagged <- tryCatch(
-    udpipe(utf8_docs, model,
-           model_dir = pid.pos_env$model_folder,
-           doc_id = doc_id
-    ),
-    error = function(e) stop("UDPipe Model can't be loaded/ downloaded.
+  names(utf8_docs) <- .doc_id
+  
+  if (catch){
+    tagged <- tryCatch(
+      udpipe(utf8_docs, model,
+             model_dir = pid.pos_env$model_folder
+      ),
+      error = function(e) stop("UDPipe Model can't be loaded/ downloaded.
                              Please run `browse_model_location()` to see if models are downloaded
                              and if not present download via `browse_udpipe_repo()`.")
-  )
-
-  if (!is.null(pb)){
-    pb$tick()
+    )
+  } else {
+    tagged <- udpipe(utf8_docs, model,
+                     model_dir = pid.pos_env$model_folder
+    )
   }
 
   mutate(tagged, `Token No` = as.numeric(.data$`token_id`))
 
 }
 
-chunked_pos_tag <- function(docs, model="english-ewt", chunk_size=100, pb=NULL,
+chunked_pos_tag <- function(docs, model="english-ewt", chunk_size=100,
                             doc_ids = NULL){
   #' @importFrom purrr map2
   #' @noRd
@@ -46,7 +51,9 @@ chunked_pos_tag <- function(docs, model="english-ewt", chunk_size=100, pb=NULL,
   ids <- split(doc_ids, splits)
 
   pos_tagged <- map2(
-    jobs, ids, function(docs, ids) pos_tag(docs, model, pb, ids)
+    jobs, ids, 
+    function(docs, ids) pos_tag(docs, model, ids),
+    .progress=T
   )
 
   pos_tagged
