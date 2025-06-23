95f6184d8ed47549cd1fb6230b5f4ad7ec000a77
Rob <rzc1@staff.staffs.ac.uk>
2025-06-23 15:15:11 +0100

    Implement paper pipeline

---
+++ prepare_paper.R
@@ -0,0 +1,8 @@
+rmarkdown::render("paper.Rmd")
+
+preamble <- readr::read_lines("preamble.txt")
+
+c(preamble, readr::read_lines("paper.md")) |>
+  stringr::str_replace_all("\\\\\\[", "\\[") |>
+  stringr::str_replace_all("\\\\\\]", "\\]") |>
+  readr::write_lines("paper.md")
