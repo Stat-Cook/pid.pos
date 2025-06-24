a1c9bc9825f7998764273295e612431719d10b59
Rob <rzc1@staff.staffs.ac.uk>
2025-06-24 12:37:51 +0100

    Change bib directory.

--- prepare_paper.R
+++
@@ -1,8 +0,0 @@
-rmarkdown::render("paper.Rmd")
-
-preamble <- readr::read_lines("preamble.txt")
-
-c(preamble, readr::read_lines("paper.md")) |>
-  stringr::str_replace_all("\\\\\\[", "\\[") |>
-  stringr::str_replace_all("\\\\\\]", "\\]") |>
-  readr::write_lines("paper.md")
