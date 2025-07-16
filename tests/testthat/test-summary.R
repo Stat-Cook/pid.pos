0fb04828f90ff28c1d31ca7a0d3935f098b2fe06
Rob <rzc1@staff.staffs.ac.uk>
2025-07-16 15:21:29 +0100

    implement summary test.

---
+++ tests/testthat/test-summary.R
@@ -0,0 +1,8 @@
+test_that("summary works", {
+  
+  report <- data_frame_report(the_one_in_massapequa)
+  .summary <- summary(report)
+  
+  expect_equal(nrow(.summary), 2)
+  expect_equal(ncol(.summary), 4)
+})
