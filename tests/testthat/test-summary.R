791f3abe76c7517168d3b50f7a969018b3e9fbbd
Stat-Cook <Stat-Cook@users.noreply.github.com>
2025-07-16 14:23:13 +0000

    Style code (GHA)

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -1,8 +1,7 @@
 test_that("summary works", {
-  
   report <- data_frame_report(the_one_in_massapequa)
   .summary <- summary(report)
-  
+
   expect_equal(nrow(.summary), 2)
   expect_equal(ncol(.summary), 4)
 })
