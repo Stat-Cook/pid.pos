5f5866c99d59fc380b8c6bcd13a4de8766df552a
Rob <rzc1@staff.staffs.ac.uk>
2025-09-18 14:22:37 +0100

    Implement merge test

---
+++ tests/testthat/test-merge_replacements.R
@@ -0,0 +1,21 @@
+test_that("merge_redactions works", {
+
+  vec <- c(letters, LETTERS)
+  
+  cached_redactions <- data.frame(If = letters, Then = "XXX")
+  
+  redacted.vec <- vector_merge_redactions(vec, cached_redactions)
+  
+  expect_true(all(
+    str_detect("[a-z]", redacted.vec, T)
+  ))
+  
+  raw.frm <- data.frame(Lower = letters, Upper = LETTERS)  
+  redacted.frm <- raw.frm |>
+    merge_redactions(cached_redactions)
+  
+  expect_true(all(
+    str_detect("[a-z]", redacted.frm$Lower, T)
+  ))
+  
+})
