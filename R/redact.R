03da19a357c578cc6801e9be67a90b4700f8476a
Rob <rzc1@staff.staffs.ac.uk>
2026-03-11 10:38:51 +0000

    export

--- R/redact.R
+++ R/redact.R
@@ -3,6 +3,7 @@
 #' @param object The object to be redacted - either a vector or data frame
 #' @param redacter A `data.frame` of redaction rules or a function created by `redaction_function_factory()`.
 #'
+#' @export
 redact <- function(object, redacter, in_batches = T, ...) {
   redacter <- parse_redacter(redacter)
 
